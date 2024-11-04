ac9565a66a7415a0e7b90e29b8aaebfd532f2263
Alexander Rink <alex@rcvd.io>
2024-11-04 19:45:48 +0100

    Fixed build

--- add_css.sh
+++ add_css.sh
@@ -2,5 +2,5 @@
 cp src/build.ts src/index.ts
 
 echo "function initCSSTheme() {" >> src/index.ts
-echo "    css['blueprint'] = '`cat ../src/themes/roam.css | tr '\n' ' '`';" >> src/index.ts
+echo "    css['blueprint'] = '`cat src/theme/blueprint.css | tr '\n' ' '`';" >> src/index.ts
 echo "}" >> src/index.ts
